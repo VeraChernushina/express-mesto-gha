95e8ac655e011c7818af08640d0269c7838c2ed4
Vera Chernushina <vera.chernushina@yandex.ru>
2022-03-16 10:19:34 +0300

    refactor: delete middleware with id

--- app.js
+++ app.js
@@ -16,14 +16,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '621d46ec4532f6df2e3f2aa5',
-  };
-
-  next();
-});
-
 app.post('/signin', login);
 app.post('/signup', createUser);
 
