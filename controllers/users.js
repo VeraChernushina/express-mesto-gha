cead009243b18c501a3fbc4abd5ed8572a566249
Vera Chernushina <vera.chernushina@yandex.ru>
2022-03-19 16:30:43 +0300

    refactor: централизованная обработка ошибок

--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,21 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const ErrorConflict = require('../errors/ErrorConflict');
-const ValidationError = require('../errors/ValidationError');
-
-const { ERROR_NOT_FOUND, errorsHandler } = require('../utils/utils');
+const ConflictError = require('../errors/ConflictError');
+const BadRequestError = require('../errors/BadRequestError');
+const NotFoundError = require('../errors/NotFoundError');
 
 // Создание нового пользователя
 module.exports.createUser = (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return next(new ValidationError('Неправильный логин или пароль.'));
+    return next(new BadRequestError('Неправильный логин или пароль.'));
   }
 
   return User.findOne(email).then((user) => {
     if (user) {
-      throw new ErrorConflict(`Пользователь с ${email} уже существует.`);
+      return next(new ConflictError(`Пользователь с ${email} уже существует.`));
     }
 
     return bcrypt.hash(password, 10);
@@ -35,14 +34,24 @@ module.exports.createUser = (req, res, next) => {
       _id: user._id,
       email: user.email,
     }))
-    .catch(next);
+    .catch((err) => {
+      if (err.code === 11000) {
+        return next(new ConflictError('Пользователь с таким email уже существует.'));
+      }
+      return next(err);
+    });
 };
 
 // Аутентификация пользователя
-module.exports.login = (req, res) => {
+module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
+      // проверим существует ли такой email или пароль
+      if (!user || !password) {
+        return next(new BadRequestError('Неверный email или пароль.'));
+      }
+
       // создадим токен
       const token = jwt.sign(
         { _id: user._id },
@@ -52,12 +61,9 @@ module.exports.login = (req, res) => {
       );
 
       // вернём токен
-      res.send({ token });
+      return res.send({ token });
     })
-    .catch((err) => {
-      // возвращаем ошибку аутентификации
-      res.status(401).send({ message: err.message });
-    });
+    .catch(next);
 };
 
 // возвращает информацию о текущем пользователе
@@ -66,39 +72,35 @@ module.exports.getCurrentUser = (req, res, next) => {
   User.findById(_id).then((user) => {
     // проверяем, есть ли пользователь с таким id
     if (!user) {
-      return Promise.reject(new Error('Пользователь не найден.'));
+      return next(new NotFoundError('Пользователь не найден.'));
     }
 
     // возвращаем пользователя, если он есть
     return res.status(200).send(user);
-  }).catch((err) => {
-    next(err);
-  });
+  }).catch(next);
 };
 
 // Получение пользователей
-module.exports.getUsers = (req, res) => {
+module.exports.getUsers = (req, res, next) => {
   User.find({})
     .then((users) => res.status(200).send(users))
-    .catch((err) => errorsHandler(err, res));
+    .catch(next);
 };
 
 // Получение пользователя по его id
-module.exports.getUserById = (req, res) => {
+module.exports.getUserById = (req, res, next) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (!user) {
-        return res
-          .status(ERROR_NOT_FOUND)
-          .send({ message: 'Пользователь не найден' });
+        return next(new NotFoundError('Пользователь не найден'));
       }
       return res.status(200).send(user);
     })
-    .catch((err) => errorsHandler(err, res));
+    .catch(next);
 };
 
 // Обновление информации о пользователе
-module.exports.updateUser = (req, res) => {
+module.exports.updateUser = (req, res, next) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(
     req.user._id,
@@ -106,11 +108,16 @@ module.exports.updateUser = (req, res) => {
     { new: true, runValidators: true },
   )
     .then((user) => res.status(200).send(user))
-    .catch((err) => errorsHandler(err, res));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return next(new BadRequestError('Неверный тип данных.'));
+      }
+      return next(err);
+    });
 };
 
 // Обновление аватара пользователя
-module.exports.updateAvatar = (req, res) => {
+module.exports.updateAvatar = (req, res, next) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(
     req.user._id,
@@ -118,5 +125,10 @@ module.exports.updateAvatar = (req, res) => {
     { new: true, runValidators: true },
   )
     .then((user) => res.status(200).send(user))
-    .catch((err) => errorsHandler(err, res));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return next(new BadRequestError('Неверная ссылка'));
+      }
+      return next(err);
+    });
 };
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
+const UnathorizedError = require('../errors/UnathorizedError');
 
-// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   // достаём авторизационный заголовок
   const { authorization } = req.headers;
 
   // убеждаемся, что он есть или начинается с Bearer
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    throw new UnathorizedError('Необходима авторизация.');
   }
 
   // извлечём токен
@@ -19,9 +19,7 @@ module.exports = (req, res, next) => {
     payload = jwt.verify(token);
   } catch (err) {
     // отправим ошибку, если не получилось
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    throw new UnathorizedError('Необходима авторизация.');
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
