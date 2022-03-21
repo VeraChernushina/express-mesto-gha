c904e2bbaf96618dfc443c82070cb59f968b6fc3
Vera Chernushina <chernushina.vera@gmail.com>
2022-03-21 23:20:36 +0300

    fix: валидация url аватара с помощью joi

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -1,4 +1,6 @@
 const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
+const BadRequestError = require('../errors/BadRequestError');
 
 const signIn = celebrate({
   body: Joi.object().keys({
@@ -13,6 +15,12 @@ const signUp = celebrate({
     password: Joi.string().required().min(8).max(30),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
+    avatar: Joi.string().custom((value) => {
+      if (!validator.isURL(value, { require_protocol: true })) {
+        throw new BadRequestError('Неправильный формат URL адреса');
+      }
+      return value;
+    }),
   }),
 });
 
